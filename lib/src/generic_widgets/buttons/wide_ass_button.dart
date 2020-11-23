bdbdbaa8f5f0edef04cfde49a60ff17dbce2e8d5
Mircea Anton <mircea.anton99@yahoo.com>
2020-11-23 20:51:10 +0000

    Implement Wide FlatButton

---
+++ lib/src/generic_widgets/buttons/wide_ass_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class WideAssButton extends StatelessWidget {
+  final void Function() onPress;
+  final String text;
+
+  const WideAssButton({
+    @required this.onPress,
+    @required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.headline5.copyWith(
+              color: Theme.of(context).accentColor,
+            ),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 64.0,
+        vertical: 16.0,
+      ),
+      onPressed: onPress,
+    );
+  }
+}
