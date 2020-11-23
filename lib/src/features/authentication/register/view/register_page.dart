6e7142d69cc5ccee4b41dcb541029aaeb2d52a40
Mircea Anton <mircea.anton99@yahoo.com>
2020-11-23 22:42:34 +0000

    Implement BLoC widgets

--- lib/src/features/authentication/register/view/register_page.dart
+++ lib/src/features/authentication/register/view/register_page.dart
@@ -1,33 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/src/features/_generics_/code/snackbar.dart';
+import 'package:frontend/src/features/_generics_/widgets/loading_indicator.dart';
 
-import 'package:frontend/src/features/authentication/register/view/widgets/register_banner.dart';
-import 'package:frontend/src/features/authentication/register/view/widgets/register_form.dart';
-import 'package:frontend/src/generic_widgets/background_pattern.dart';
-import 'package:frontend/src/generic_widgets/banner_form_dialog.dart';
+import '../bloc/register_bloc.dart';
+import 'widgets/register_dialog.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage();
 
+  Widget _builder(BuildContext context, RegisterState state) {
+    if (state is RegisterLoading) {
+      return const LoadingIndicator();
+    } else if (state is RegisterSuccess) {
+      Navigator.pushReplacementNamed(context, "/login");
+    } else if (state is RegisterFail || state is RegisterInitial) {
+      return const RegisterDialog();
+    }
+    return const SizedBox(width: 1.0, height: 1.0);
+  }
+
+  void _listener(BuildContext context, RegisterState state) {
+    if (state is RegisterSuccess) {
+      showSnackbar(
+        context: context,
+        content: "Register Successful. You can now Log In.",
+      );
+    } else if (state is RegisterFail) {
+      showSnackbar(
+        context: context,
+        content: "Register Failed",
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(Theme.of(context).brightness);
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          BackgroundPattern(opacity: 0.4),
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 120.0,
-              vertical: 60.0,
-            ),
-            child: BannerFormDialog(
-              banner: RegisterBanner(flex: 2),
-              form: RegisterForm(flex: 3),
-            ),
-          )
-        ],
+    return BlocProvider(
+      create: (context) => RegisterBloc(),
+      child: BlocConsumer<RegisterBloc, RegisterState>(
+        listener: _listener,
+        builder: _builder,
       ),
     );
   }
