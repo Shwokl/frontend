1c876ebed78a43629a25dac3aec65e7ef94968f3
Mircea Anton <mircea.anton99@yahoo.com>
2020-11-23 09:46:08 +0000

    Restructure main.dart

---
+++ lib/src/app.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'features/authentication/login/login_page.dart';
+import 'features/theming/barrell.dart';
+
+class ShwoklApp extends StatelessWidget {
+  const ShwoklApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeBloc>(
+          create: (context) => ThemeBloc(AppTheme.Dark),
+        ),
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: _buildWithTheme,
+      ),
+    );
+  }
+
+  Widget _buildWithTheme(BuildContext context, ThemeState state) {
+    return MaterialApp(
+      title: 'Flukabo',
+      theme: state.themeData,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+      },
+    );
+  }
+}
