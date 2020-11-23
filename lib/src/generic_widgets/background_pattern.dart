fd644bd7548bdaae9d782aeab58e30566345671f
Mircea Anton <mircea.anton99@yahoo.com>
2020-11-23 20:51:45 +0000

    Implement custom widget for background patterns

---
+++ lib/src/generic_widgets/background_pattern.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BackgroundPattern extends StatelessWidget {
+  final double opacity;
+  const BackgroundPattern({this.opacity});
+
+  @override
+  Widget build(BuildContext context) {
+    String _pattern = "lib/src/assets/pattern/";
+    if (Theme.of(context).brightness == Brightness.dark) {
+      _pattern += "dark.png";
+    } else {
+      _pattern += "light.png";
+    }
+
+    return Opacity(
+      opacity: opacity,
+      child: Image.asset(
+        _pattern,
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+      ),
+    );
+  }
+}
