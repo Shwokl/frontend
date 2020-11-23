4e5a2f3338b9fc1bcf520456752fa2d5a4576974
Mircea Anton <mircea.anton99@yahoo.com>
2020-11-23 22:15:01 +0000

    Implement register BLoC outline

---
+++ lib/src/features/authentication/register/bloc/register_bloc.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/src/features/_generics_/failure.dart';
+
+part 'register_event.dart';
+part 'register_state.dart';
+
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  RegisterBloc() : super(RegisterInitial());
+
+  @override
+  Stream<RegisterState> mapEventToState(RegisterEvent event) async* {
+    yield RegisterLoading();
+    try {
+      // todo API register attempt
+      yield RegisterSuccess();
+    } on Failure catch (f) {
+      yield RegisterFail(f.message);
+    }
+  }
+}
---
+++ lib/src/features/authentication/register/bloc/register_event.dart
@@ -0,0 +1,23 @@
+part of 'register_bloc.dart';
+
+abstract class RegisterEvent extends Equatable {
+  final String name;
+  final String username;
+  final String email;
+  final String password;
+
+  const RegisterEvent({
+    @required this.name,
+    @required this.username,
+    @required this.email,
+    @required this.password,
+  });
+
+  @override
+  List<Object> get props => [
+        this.name,
+        this.username,
+        this.email,
+        this.password,
+      ];
+}
