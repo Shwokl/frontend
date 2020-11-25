697bb32bd203b777db96da1058e2a99e6c931f24
Mircea Anton <mircea.anton99@yahoo.com>
2020-11-26 01:37:27 +0200

    Send API request for login

--- lib/src/features/authentication/login/bloc/login_bloc.dart
+++ lib/src/features/authentication/login/bloc/login_bloc.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/src/models/singletons/Shwokl_API.dart';
+import 'package:http/http.dart';
 
 import 'package:frontend/src/features/_generics_/code/failure.dart';
 
@@ -10,15 +12,23 @@ part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial());
+  LoginBloc() : super(const LoginInitial());
 
   @override
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
-    yield LoginLoading();
+    yield const LoginLoading();
     try {
-      // todo API login attempt
-      await Future.delayed(Duration(seconds: 1));
-      yield LoginSuccess();
+      final Response response = await ShwoklAPI().sendRequest(
+        url: "http://192.168.15.51:8080/api/v1/login",
+        user: event.username,
+        token: event.password,
+        method: null,
+      );
+      if (response.statusCode == 200) {
+        yield const LoginSuccess();
+      } else {
+        yield LoginFail(response.body);
+      }
     } on Failure catch (f) {
       yield LoginFail(f.message);
     }
