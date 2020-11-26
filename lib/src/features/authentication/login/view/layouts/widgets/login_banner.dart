dae7666cdbba457d12d4eb97d2b48b5d7c0a1cba
Mircea Anton <mircea.anton99@yahoo.com>
2020-11-26 16:54:31 +0200

    Add toggle to show/hide banner image

--- lib/src/features/authentication/login/view/layouts/widgets/login_banner.dart
+++ lib/src/features/authentication/login/view/layouts/widgets/login_banner.dart
@@ -3,7 +3,8 @@ import 'package:frontend/src/generic_widgets/buttons/wide_ass_button.dart';
 import 'package:frontend/src/generic_widgets/dialog_banner.dart';
 
 class LoginBanner extends StatelessWidget {
-  const LoginBanner();
+  final bool showImage;
+  const LoginBanner({this.showImage = true});
 
   void _onSignupPress(BuildContext context) {
     Future.delayed(Duration.zero).then(
@@ -14,7 +15,7 @@ class LoginBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DialogBanner(
-      imageSrc: 'lib/src/assets/welcome.png',
+      imageSrc: showImage ? "lib/src/assets/welcome.png" : "",
       prompt: "Not a member?",
       button: WideAssButton(
         text: "Sign Up",
