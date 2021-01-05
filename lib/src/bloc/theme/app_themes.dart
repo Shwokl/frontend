05a7ce4c2ac624ed75828ac23f6190681a0be35d
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-05 21:59:35 +0000

    Reset Light theme & Change dark theme

--- lib/src/bloc/theme/app_themes.dart
+++ lib/src/bloc/theme/app_themes.dart
@@ -10,16 +10,19 @@ enum AppTheme { Dark, Light }
 /// The light theme emulates the Material Theme Lighter High Contrast theme from
 /// VS Code.
 final Map<AppTheme, ThemeData> appThemeData = {
-  AppTheme.Dark: ThemeData.dark().copyWith(
-    primaryColorDark: const Color(0xFF1d2d50),
-    primaryColor: const Color(0xFF133b5c),
-    primaryColorLight: const Color(0xFF1e5f74),
-    accentColor: const Color(0xFFfcdab7),
+  AppTheme.Dark: ThemeData(
+    brightness: Brightness.dark,
+    primaryColorDark: const Color(0xFF000000),
+    primaryColor: const Color(0xFF212121),
+    primaryColorLight: const Color(0xFF424242),
+    accentColor: const Color(0xFFE1DAC1),
+    fontFamily: 'Poppins',
   ),
-  AppTheme.Light: ThemeData.light().copyWith(
-    primaryColorDark: const Color(0xFFd37815),
-    primaryColor: const Color(0xFFcad315),
-    primaryColorLight: const Color(0xFFe4e978),
-    accentColor: const Color(0xFFf2f4c0),
+  AppTheme.Light: ThemeData(
+    brightness: Brightness.light,
+    primaryColor: const Color(0xFF000000),
+    primaryColorDark: const Color(0xFF000000),
+    accentColor: const Color(0xFF000000),
+    fontFamily: 'Poppins',
   ),
 };
