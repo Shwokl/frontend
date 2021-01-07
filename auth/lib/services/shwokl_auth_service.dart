ddf588c640988aa9c8ffa390ffeebcde02641b10
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-07 17:51:24 +0000

    Implement authentication methods

--- auth/lib/services/shwokl_auth_service.dart
+++ auth/lib/services/shwokl_auth_service.dart
@@ -1,5 +1,6 @@
 // External imports
 import 'package:async/async.dart' show Result;
+import 'package:flutter/foundation.dart' show required;
 
 // Local imports
 import '../api/generic_auth_api.dart';
@@ -14,20 +15,49 @@ class ShwoklAuthService implements GenericAuthService, GenericSignupService {
   const ShwoklAuthService(this._api);
 
   @override
-  Future<Result<Token>> signIn(Credentials credentials) {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<Result<Token>> signIn(final Credentials credentials) async {
+    assert(credentials != null);
+
+    final Result<String> result = await _api.singIn(credentials);
+
+    if (result.isError) {
+      return result.asError;
+    } else {
+      return Result.value(Token(result.asValue.value));
+    }
   }
 
   @override
-  Future<Result<bool>> signOut(Token token) {
-    // TODO: implement signOut
-    throw UnimplementedError();
+  Future<Result<bool>> signOut(Token token) async {
+    assert(token != null && token.value.isNotEmpty);
+    return _api.signOut(token);
   }
 
   @override
-  Future<Result<Token>> signup() {
-    // TODO: implement signup
-    throw UnimplementedError();
+  Future<Result<Token>> signup({
+    final String name = "",
+    @required final String username,
+    @required final String password,
+    @required final String email,
+  }) async {
+    assert(username.isNotEmpty && username != null);
+    assert(password.isNotEmpty && password != null);
+    assert(email.isNotEmpty && email != null);
+
+    final Credentials credentials = Credentials(
+      method: AuthMethod.email,
+      email: email,
+      name: name,
+      username: username,
+      password: password,
+    );
+
+    final Result<String> result = await _api.singUp(credentials);
+
+    if (result.isError) {
+      return result.asError;
+    } else {
+      return Result.value(Token(result.asValue.value));
+    }
   }
 }
