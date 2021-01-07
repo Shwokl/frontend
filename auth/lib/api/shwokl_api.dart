195306234256f15efb44b80597ae0983f057b5e0
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-07 17:48:14 +0000

    Implement method to send post requests

--- auth/lib/api/shwokl_api.dart
+++ auth/lib/api/shwokl_api.dart
@@ -1,5 +1,7 @@
 // External imports
+import 'dart:convert' show jsonDecode;
 import 'package:async/async.dart' show Result;
+import 'package:flutter/foundation.dart' show required;
 import 'package:http/http.dart' as http;
 
 // Local imports
@@ -29,5 +31,26 @@ class ShwoklApi implements GenericAuthApi {
   Future<Result<String>> singUp(Credentials credentials) {
     // TODO: implement singUp
     throw UnimplementedError();
+  }
+
+  Future<Result<String>> _sendPost({
+    @required final String url,
+    final Map<String, String> header = const {
+      "Content-type": "application/json"
+    },
+    final Map<String, dynamic> body = const {},
+  }) async {
+    final http.Response response =
+        await _client.post(url, body: body, headers: header);
+
+    final dynamic json = jsonDecode(response.body);
+    if (response.statusCode != 200) {
+      return Result.error(json['message']);
+    }
+    if (json['token'] == null) {
+      return Result.error(json['message']);
+    } else {
+      return Result.value(json['token'].toString());
+    }
   }
 }
