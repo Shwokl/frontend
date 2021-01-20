070d1b7ab6443fad34f3e7e9029ce016a4511a93
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-20 14:22:39 +0200

    Get user as argument

--- lib/src/ui/pages/home/views/web_view.dart
+++ lib/src/ui/pages/home/views/web_view.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/src/ui/utils/navigation.dart';
-import 'package:frontend/src/ui/widgets/custom_scaffolds/background_scaffold/background_scaffold.dart';
-import 'package:frontend/src/ui/widgets/custom_scaffolds/custom_appbar/custom_appbar.dart';
-import 'package:frontend/src/ui/widgets/input_fields/buttons/custom/settings_button.dart';
-import 'package:frontend/src/ui/widgets/input_fields/buttons/custom/theme_button.dart';
-import 'package:frontend/src/ui/widgets/input_fields/buttons/generic/nav_button.dart';
+
+// Local imports
+import '../../../../data/shwokl/users/user.dart';
+import '../../../../ui/utils/navigation.dart';
+import '../../../../ui/widgets/custom_scaffolds/background_scaffold/background_scaffold.dart';
+import '../../../../ui/widgets/custom_scaffolds/custom_appbar/custom_appbar.dart';
+import '../../../../ui/widgets/input_fields/buttons/custom/settings_button.dart';
+import '../../../../ui/widgets/input_fields/buttons/custom/theme_button.dart';
+import '../../../../ui/widgets/input_fields/buttons/generic/nav_button.dart';
+import '../../../../ui/widgets/templates/user_card/user_card.dart';
 
 class WebView extends StatelessWidget {
+  final User user;
+
+  const WebView({
+    @required this.user,
+  });
+
   @override
   Widget build(BuildContext context) {
     return BackgroundScaffold(
@@ -22,6 +32,13 @@ class WebView extends StatelessWidget {
           ThemeButton(),
           SettingsButton(),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Row(
+          children: [
+            UserCard(user),
+          ],
+        ),
       ),
     );
   }
