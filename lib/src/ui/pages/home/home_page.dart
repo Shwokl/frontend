967d9701f2be210b892b53aa1befee6bd277c5a6
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-20 14:22:48 +0200

    send dummy data to view

--- lib/src/ui/pages/home/home_page.dart
+++ lib/src/ui/pages/home/home_page.dart
@@ -1,7 +1,9 @@
 // External imports
 import 'package:flutter/material.dart';
+import 'package:frontend/src/data/shwokl/roles/role.dart';
 
 // Local imports
+import '../../../data/shwokl/users/user.dart';
 import 'views/web_view.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,6 +11,16 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WebView();
+    return WebView(
+      user: User(
+          id: 1,
+          avatar:
+              'https://images.unsplash.com/photo-1536164261511-3a17e671d380?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2130&q=80',
+          username: 'mike',
+          password: 'password',
+          spice: 'spice',
+          role: Role(id: 2, name: 'User'),
+          name: 'Michael Anthony'),
+    );
   }
 }
