385224eea69f8ca568d3fc2e1f3a922a8d6bff6d
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-20 21:56:52 +0200

    Add more dummy data

--- lib/src/ui/pages/home/home_page.dart
+++ lib/src/ui/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 // External imports
 import 'package:flutter/material.dart';
+import 'package:frontend/src/data/shwokl/workout_plans/workout_plan.dart';
 
 // Local imports
 import '../../../data/shwokl/roles/role.dart';
@@ -12,54 +13,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      user: User(
-        id: 1,
-        avatar:
-            'https://images.unsplash.com/photo-1536164261511-3a17e671d380?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2130&q=80',
-        username: 'mike',
-        password: 'password',
-        spice: 'spice',
-        role: Role(id: 2, name: 'User'),
-        name: 'Michael Anthony',
+    final User user = User(
+      id: 1,
+      avatar:
+          'https://images.unsplash.com/photo-1536164261511-3a17e671d380?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2130&q=80',
+      username: 'mike',
+      password: 'password',
+      spice: 'spice',
+      role: Role(id: 2, name: 'User'),
+      name: 'Michael Anthony',
+    );
+
+    final WorkoutPlan planTemplate = WorkoutPlan(
+      id: 1,
+      image: 'https://source.unsplash.com/random',
+      name: 'My workout plan',
+      creator: User(
+        name: 'John Doe',
+        id: 2,
+        password: '',
+        role: null,
+        spice: '',
+        username: 'john.doe',
       ),
-      logs: [
-        WorkoutLog(
-            id: 1,
-            user: null,
-            name: '',
-            date: DateTime.fromMillisecondsSinceEpoch(1578780000000)),
-        WorkoutLog(
-            id: 1,
-            user: null,
-            name: '',
-            date: DateTime.fromMillisecondsSinceEpoch(1578952800000)),
-        WorkoutLog(
-            id: 1,
-            user: null,
-            name: '',
-            date: DateTime.fromMillisecondsSinceEpoch(1579039200000)),
-        WorkoutLog(
-            id: 1,
-            user: null,
-            name: '',
-            date: DateTime.fromMillisecondsSinceEpoch(1579125600000)),
-        WorkoutLog(
-            id: 1,
-            user: null,
-            name: '',
-            date: DateTime.fromMillisecondsSinceEpoch(1579471200000)),
-        WorkoutLog(
-            id: 1,
-            user: null,
-            name: '',
-            date: DateTime.fromMillisecondsSinceEpoch(1578607200000)),
-        WorkoutLog(
-            id: 1,
-            user: null,
-            name: '',
-            date: DateTime.fromMillisecondsSinceEpoch(1578434400000)),
+    );
+
+    final WorkoutLog a = WorkoutLog(
+      id: 1,
+      user: null,
+      name: '',
+      date: DateTime(2021, 1, 10),
+    );
+    final WorkoutLog b = WorkoutLog(
+      id: 1,
+      user: null,
+      name: '',
+      date: DateTime(2021, 1, 12),
+    );
+    final WorkoutLog c = WorkoutLog(
+      id: 1,
+      user: null,
+      name: '',
+      date: DateTime(2021, 1, 13),
+    );
+    final WorkoutLog d = WorkoutLog(
+      id: 1,
+      user: null,
+      name: '',
+      date: DateTime(2021, 1, 17),
+    );
+
+    return WebView(
+      user: user,
+      plans: [
+        planTemplate,
+        planTemplate,
+        planTemplate,
+        planTemplate,
+        planTemplate,
+        planTemplate,
+        planTemplate,
+        planTemplate,
       ],
+      logs: [a, b, c, d],
     );
   }
 }
