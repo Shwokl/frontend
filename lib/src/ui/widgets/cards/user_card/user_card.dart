979a0b02bef412e360f874757d4bed86a1792eb2
Mircea Anton <mircea.anton99@yahoo.com>
2021-01-20 16:31:14 +0200

    Relocate files

---
+++ lib/src/ui/widgets/cards/user_card/user_card.dart
@@ -0,0 +1,43 @@
+// External imports
+import 'package:flutter/material.dart';
+
+// Local imports
+import '../../../../data/shwokl/users/user.dart';
+import 'components/all.dart';
+
+class UserCard extends StatelessWidget {
+  final User user;
+  const UserCard(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        width: 500,
+        height: 150,
+        child: Row(
+          children: [
+            UserAvatar(user.avatar),
+            const SizedBox(width: 24.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    NameBox(user.name),
+                    const SizedBox(width: 8.0),
+                    RoleIcon(user.role.id),
+                  ],
+                ),
+                UsernameBox(user.username),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
